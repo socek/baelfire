33ab24f7ec0302ae177e66d3c04a17ff6c1e6917
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2014-03-30 15:09:22 +0200

    Test for TemplateTask. #baelfire-1 Done

--- baelfire/template.py
+++ baelfire/template.py
@@ -12,38 +12,44 @@ class TemplateTask(Task):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._env = None
+        self._jinja = None
 
-    def get_module(self):
+    def module(self):
+        """Module which store templates."""
         return sys.modules[self.recipe.__module__]
 
-    def get_template_full_path(self):
-        dirname = os.path.dirname(self.get_module().__file__)
+    def template_absolute_path(self):
+        """Absoluth path to a template."""
+        dirname = os.path.dirname(self.module().__file__)
         return os.path.join(dirname,
                             self.templates_dir,
                             self.get_template_path())
 
     def _generate_dependencys(self):
-        self.add_dependecy(FileChanged(self.get_template_full_path()))
-        self.add_dependecy(FileChanged(self.get_module().__file__))
+        """Generates FileChanged dependency for template file and task file.
+        generate_dependencys method is not needed now."""
+        self.add_dependecy(FileChanged(self.template_absolute_path()))
+        task_file = sys.modules[self.__module__].__file__
+        self.add_dependecy(FileChanged(task_file))
 
         getattr(self, 'generate_dependencys', lambda: None)()
 
-    def get_env(self):
-        if self._env is None:
-            loader = PackageLoader(self.get_module().__name__,
+    def jinja(self):
+        """Jinja2 environment generator."""
+        if self._jinja is None:
+            loader = PackageLoader(self.module().__name__,
                                    self.templates_dir)
-            self._env = Environment(loader=loader)
-        return self._env
+            self._jinja = Environment(loader=loader)
+        return self._jinja
 
-    def get_data(self):
+    def generate_data(self):
+        """Generates data which will be used in the template."""
         data = {}
         data['settings'] = self.settings
         data['paths'] = self.paths
         return data
 
     def make(self):
-        env = self.get_env()
-        template = env.get_template(self.get_template_path())
-        stream = template.stream(**self.get_data())
+        template = self.jinja().get_template(self.get_template_path())
+        stream = template.stream(**self.generate_data())
         stream.dump(self.get_output_file())
--- baelfire/tests/template.py
+++ baelfire/tests/template.py
@@ -1,3 +1,8 @@
+import os
+import sys
+import shutil
+from jinja2 import Environment
+
 from mock import MagicMock
 from soktest import TestCase
 
@@ -6,8 +11,17 @@ from baelfire.template import TemplateTask
 
 class TemplateTaskExample(TemplateTask):
 
+    def get_output_file(self):
+        dirname = os.path.dirname(self.template_absolute_path())
+        return os.path.join(dirname, 'myfile')
+
     def get_template_path(self):
         return 'template.jinja2'
+
+    def generate_data(self):
+        data = super().generate_data()
+        data['mytest'] = 'success'
+        return data
 
 
 class TemplateTaskTest(TestCase):
@@ -18,5 +32,77 @@ class TemplateTaskTest(TestCase):
         self.template = TemplateTaskExample()
         self.template.assign_recipe(self.recipe)
 
-    def test_simple(self):
-        self.template.get_env()
+    def test_module(self):
+        """Should return mock module, because recipe is a MagicMock."""
+        expected_module = sys.modules['mock']
+        self.assertEqual(expected_module, self.template.module())
+
+    def test_template_absolute_path(self):
+        """Should return absolute path for a template."""
+        self.add_mock_object(self.template, 'module')
+        self.mocks['module'].return_value.__file__ = '/main/child'
+        path = self.template.template_absolute_path()
+        self.assertEqual('/main/templates/template.jinja2', path)
+
+    def test_generate_dependencys(self):
+        """Should add FileChanged dependency for template file and this file.
+        """
+        self.add_mock_object(self.template, 'module')
+        self.mocks['module'].return_value.__file__ = '/main/child'
+
+        self.template._generate_dependencys()
+
+        dependency = self.template.dependencys[0]
+        self.assertEqual(
+            ['/main/templates/template.jinja2'], dependency.filenames)
+
+        dependency = self.template.dependencys[1]
+        self.assertEqual([__file__], dependency.filenames)
+
+    def test_jinja_when_new(self):
+        """Should generate new jinja2 envoritment."""
+        self.assertEqual(None, self.template._jinja)
+
+        env = self.template.jinja()
+
+        self.assertEqual(self.template._jinja, env)
+        self.assertEqual(Environment, type(env))
+
+    def test_jinja_when_exists(self):
+        """Should return jinja2 envoritment generated before."""
+        self.template._jinja = MagicMock()
+
+        env = self.template.jinja()
+
+        self.assertEqual(self.template._jinja, env)
+
+    def test_generate_data(self):
+        """Should return dict with settings and paths from recipe."""
+        data = self.template.generate_data()
+
+        self.assertEqual(self.recipe.settings, data['settings'])
+        self.assertEqual(self.recipe.paths, data['paths'])
+
+    def test_make(self):
+        """Should generate template from given file."""
+        try:
+            self.add_mock_object(
+                self.template, 'module', return_value=sys.modules[__name__])
+            self.template.templates_dir = 'testdir'
+            test_path = os.path.dirname(self.template.template_absolute_path())
+            os.mkdir(test_path)
+            template = open(self.template.template_absolute_path(), 'w')
+            template.write("This is sample {{mytest}} template.")
+            template.close()
+
+            self.template.make()
+
+            _file = open(self.template.get_output_file(), 'r')
+            generated_data = _file.read()
+            _file.close()
+
+            self.assertEqual(
+                'This is sample success template.', generated_data)
+
+        finally:
+            shutil.rmtree(test_path, True)

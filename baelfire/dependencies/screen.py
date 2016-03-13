991c5f30b8017ce2d3d2e6c1ab07082b3513d94a
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2016-03-13 21:58:41 +0100

    Add Screen tasks.

---
+++ baelfire/dependencies/screen.py
@@ -0,0 +1,42 @@
+from subprocess import PIPE
+from subprocess import Popen
+import re
+
+from .dependency import Dependency
+
+
+class ScreenIsNotRunning(Dependency):
+    regex = re.compile(r'^\s*(.*)\.(\w+)\s*\((\w+)\)$', re.UNICODE)
+
+    def __init__(self, screen_name=None):
+        super().__init__()
+        self._screen_name = screen_name
+
+    def _list_screen(self):
+        cmd = self.paths['exe:screen'] + ' -list'
+        spp = Popen([cmd], shell=True, stdout=PIPE)
+        spp.wait()
+        return spp.stdout
+
+    def get_screens(self):
+        for line in self._list_screen().readlines():
+            line = line.decode()
+            try:
+                data = self.regex.findall(str(line))[0]
+                yield {
+                    'pid': int(data[0]),
+                    'name': data[1],
+                    'status': data[2],
+                }
+            except IndexError:
+                pass
+
+    def should_build(self):
+        for screen in self.get_screens():
+            if screen['name'] == self.screen_name:
+                return False
+        return True
+
+    @property
+    def screen_name(self):
+        return self._screen_name or self.parent.screen_name
---
+++ baelfire/task/screen.py
@@ -0,0 +1,36 @@
+from baelfire.dependencies.dependency import AlwaysRebuild
+from baelfire.dependencies.task import RunBefore
+from baelfire.task import SubprocessTask
+
+
+class ScreenTask(SubprocessTask):
+
+    screen_name = None
+
+    def phase_settings(self):
+        super().phase_settings()
+        self.paths['exe:screen'] = ['/usr', 'bin', 'screen']
+
+    def _screen_run(self, *args, **kwargs):
+        screen_name = 'S %s' % (self.screen_name,)
+        args[0][0] = (
+            '%s -dm%s ' % (self.paths['exe:screen'], screen_name) + args[0][0]
+        )
+        return self.popen(*args, **kwargs)
+
+
+class RetachScreenTask(SubprocessTask):
+    detached_task = None
+
+    def create_dependecies(self):
+        self._detached_task = self.detached_task()
+
+        self.add_dependency(RunBefore(self._detached_task))
+        self.add_dependency(AlwaysRebuild())
+
+    def _screen_detach(self):
+        cmd = '%s -r %s' % (self.paths['exe:screen'], self._detached_task.screen_name)
+        return self.popen([cmd])
+
+    def build(self):
+        self._screen_detach()
